323d90d262807f3ced67be33bace14b58815eb60
Jomsang <af7078@naver.com>
2021-02-16 20:34:09 +0900

    깃추가된내용 수정

--- src/Git_Test/Test_Demo.java
+++ src/Git_Test/Test_Demo.java
@@ -4,6 +4,9 @@ public class Test_Demo {
 
 	public static void main(String[] args) {
 		System.out.println("±ê Å×½ºÆ®");
+		System.out.println("±ê Å×½ºÆ®1");
+		System.out.println("±ê Å×½ºÆ®2");
+
 
 	}
 
